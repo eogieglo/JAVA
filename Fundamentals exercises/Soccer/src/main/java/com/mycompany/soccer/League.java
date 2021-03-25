16611df08ab7ea90de4a50d7bddfd54540e989c4
eogieglo <41413402+eogieglo@users.noreply.github.com>
2021-03-25 13:07:57 -0300

    Update Classes and Main League

--- Fundamentals exercises/Soccer/src/main/java/com/mycompany/soccer/League.java
+++ Fundamentals exercises/Soccer/src/main/java/com/mycompany/soccer/League.java
@@ -3,20 +3,36 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.soccer;
+
+package src.main.java.com.mycompany.soccer;
 
 /**
  *
  * @author eogieglo
  */
 public class League {
+
     public static void main(String[] args) {
-        
+
         Player player1 = new Player();
-        
         player1.playerName="George Eliot";
-        
-        
-        
+
+        Player player2 = new Player();
+        player2.playerName="Graham Greene";
+
+        Player player3 = new Player();
+        player3.playerName="Geoffrey Chaucer";
+
+        Player [] thePlayers = {player1,player2,player3};
+
+        Team team1 = new Team();
+        team1.teamName = "The Greens";
+        team1.playerArray = thePlayers;
+
+        //Output
+        for (Player thePlayer: team1.playerArray){
+            System.out.println(thePlayer.playerName);
+        }
+
     }
 }
--- Fundamentals exercises/Soccer/src/main/java/com/mycompany/soccer/Team.java
+++ Fundamentals exercises/Soccer/src/main/java/com/mycompany/soccer/Team.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.soccer;
+package src.main.java.com.mycompany.soccer;
 
 /**
  *
