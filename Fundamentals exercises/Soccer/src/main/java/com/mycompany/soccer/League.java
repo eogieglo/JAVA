045d8e100f3520dabc1b3c714df9eb2164063df8
eogieglo <41413402+eogieglo@users.noreply.github.com>
2021-03-25 13:39:59 -0300

    Update League.java

--- Fundamentals exercises/Soccer/src/main/java/com/mycompany/soccer/League.java
+++ Fundamentals exercises/Soccer/src/main/java/com/mycompany/soccer/League.java
@@ -39,10 +39,31 @@ public class League {
         team2.playerArray[2] = new Player();
         team2.playerArray[2].playerName = "Rafael Sabatini";
 
+
+        Game currGame = new Game();
+        currGame.homeTeam = team1;
+        currGame.awayTeam = team2;
+
+        Goal goal1 = new Goal();
+        goal1.thePlayer = currGame.homeTeam.playerArray[2];
+        goal1.theTeam = currGame.homeTeam;
+        goal1.theTime = 55;
+
+        Goal [] theGoals = {goal1};
+        currGame.goals = theGoals;
+
+        //Output
+
+        System.out.println(" ");
+        System.out.println("Goal scored after " +
+                currGame.goals[0].theTime + " mins by " +
+                currGame.goals[0].thePlayer.playerName + " of " +
+                currGame.goals[0].theTeam.teamName);
+
         //player1.playerName="Robert Services";
 
         //Output
-        
+        /*
         System.out.println(" ");
         System.out.println("Team 1");
         System.out.println("--------------");
@@ -58,6 +79,7 @@ public class League {
 
         for (Player thePlayer: team2.playerArray){
             System.out.println(thePlayer.playerName);
-        }
+        }*/
+
     }
 }
